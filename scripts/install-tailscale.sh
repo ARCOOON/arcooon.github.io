a6ac570c7b1f089af290c958aeaefbaf09b15f27
Leon <78073305+ARCOOON@users.noreply.github.com>
2025-08-23 16:13:04 +0200

    Update install-tailscale.sh

--- scripts/install-tailscale.sh
+++ scripts/install-tailscale.sh
@@ -2,10 +2,3 @@
 apt update
 apt install curl -y
 curl -fsSL https://tailscale.com/install.sh | sh
-
-cat <<EOF
-Add the following to /etc/pve/lxc/1xx.conf inside your pve shell.
-
-lxc.cgroup2.devices.allow: c 10:200 rwm
-lxc.mount.entry: /dev/net/tun dev/net/tun none bind,create=file
-EOF
