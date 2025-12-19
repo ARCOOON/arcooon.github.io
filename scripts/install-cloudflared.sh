8dcd2858ca5cf3d82a1be54d364105a7e45c96f1
Leon <78073305+ARCOOON@users.noreply.github.com>
2025-12-19 22:08:17 +0100

    fix(proxmox-lxc): Remove sudo from cloudflared installation script

--- scripts/install-cloudflared.sh
+++ scripts/install-cloudflared.sh
@@ -7,11 +7,11 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 # Add cloudflare gpg key
-sudo mkdir -p --mode=0755 /usr/share/keyrings
-curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
 
 # Add cloudflare stable repo to your apt repositories
-echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | tee /etc/apt/sources.list.d/cloudflared.list
 
 # Install cloudflared
 apt-get update
