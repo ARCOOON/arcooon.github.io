c4ce0ff9665ecc52437601ed37a40e23274fd151
Leon <78073305+ARCOOON@users.noreply.github.com>
2025-12-19 09:44:53 +0100

    chore: Add installation script for cloudflared.
    This script checks for root privileges, adds the Cloudflare GPG key, configures the Cloudflare repository, and installs cloudflared.

---
+++ scripts/install-cloudflared.sh
@@ -0,0 +1,18 @@
+#/usr/bin/env bash
+
+# Root check
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Error: run as root (sudo $0)" >&2
+  exit 1
+fi
+
+# Add cloudflare gpg key
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+
+# Add cloudflare stable repo to your apt repositories
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+
+# Install cloudflared
+apt-get update
+apt-get install cloudflared
