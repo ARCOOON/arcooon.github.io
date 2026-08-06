06be3cef664a1b0a52f220dbcb708afae4c7a073
Leon <78073305+ARCOOON@users.noreply.github.com>
2026-08-06 16:58:14 +0200

    chore: Enhance install-docker.sh with root check
    Added root access check and updated shebang to env.

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Checking root access
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root!"
+  exit
+fi
+
+# Updating packages
 apt update
 apt install curl -y
+
+# Installing docker engine
 curl -fsSL https://get.docker.com | sh
